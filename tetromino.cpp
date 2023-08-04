b71d97e6057a63839b1df056a7222aca2a35b04a
hannahredfive <hannah@chandleys.com>
2023-08-04 13:31:28 -0700

    Added more thoughts to Tetromino class.

---
+++ tetromino.cpp
@@ -0,0 +1,17 @@
+#include "tetromino.h"
+#include "SDL2/include/SDL.h"
+
+Tetromino::Tetromino()
+{
+
+}
+
+Tetromino::~Tetromino()
+{
+
+}
+
+void Tetromino::Draw(SDL_Renderer* renderer)
+{
+
+}
--- tetromino.h
+++ tetromino.h
@@ -4,9 +4,12 @@
 class Tetromino
 {
 public:
-	Tetromino(int window_width, int window_height);
+	Tetromino();
 	~Tetromino();
 	void Draw(SDL_Renderer*);
+	// Will need a rotate function, which requires me to make an input manager
+	// void Rotate(float dT, const InputWomanager* pInputWoman, double t);
+	
 	// max space a tetronimo can exist in, including rotation
 	static const int s_height = 4;
 	static const int s_width = 4;
@@ -18,9 +21,16 @@ private:
 
 	// Do I need shapes? Each shape has a unique color
 	// Perhaps it might be better to have Orientation
+	/*
 	enum shapes
 	{
 		o_ricky, b_ricky, c_z, ri_z, hero, teewee, smashboy
+	};
+	*/
+
+	enum orientations
+	{
+		left, right, up, down
 	};
 
 	// tiles carries the data for what color each tile of the board is
